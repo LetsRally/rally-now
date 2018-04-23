2a456a160652530a62005c903e799d906a66d352
Viacheslav-Strukov <viacheslav.strukov@p2h.com>
2018-04-23 12:30:38 +0300

    Removed firebase-analytics (it has some problems)

--- src/providers/event-logger/event-logger.ts
+++ src/providers/event-logger/event-logger.ts
@@ -1,20 +1,20 @@
 import {Injectable} from '@angular/core';
 import 'rxjs/add/operator/map';
-import {FirebaseAnalytics} from "@ionic-native/firebase-analytics";
+// import {FirebaseAnalytics} from "@ionic-native/firebase-analytics";
 
 @Injectable()
 export class EventLoggerProvider {
 
-    constructor(public fba: FirebaseAnalytics) {
+    constructor() {
         console.log('Hello EventLoggerProvider Provider');
     }
 
-    logButton(name: string, value: any) {
-        this.fba.logEvent(name, {pram: value})
-            .then((res: any) => {
-                console.log(res);
-            })
-            .catch((error: any) => console.error(error));
-    }
+    // logButton(name: string, value: any) {
+    //     this.fba.logEvent(name, {pram: value})
+    //         .then((res: any) => {
+    //             console.log(res);
+    //         })
+    //         .catch((error: any) => console.error(error));
+    // }
 
 }
