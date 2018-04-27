7bf8d73cc0aa6eb4a0d4d88210f34a461ed76c75
MayaGarnik <maya.garnik@gmail.com>
2018-04-27 11:01:40 +0300

    fix callPage

--- src/pages/call/call.ts
+++ src/pages/call/call.ts
@@ -86,7 +86,7 @@ export class CallPage {
 
     this.offices.forEach(office => {
 
-      phoneArr.push(office.phone);
+      this.phoneArr.push(office.phone);
 
       actionSheet.addButton({
         text: office.phone, 
