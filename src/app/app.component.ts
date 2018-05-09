32b8121d961ea0102ccaac3dee711cdd171c806f
Viacheslav Strukov <viacheslav.strukov@p2h.com>
2018-05-09 17:08:36 +0300

    Background color on main navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { Platform, AlertController } from 'ionic-angular';
+import {Platform, AlertController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { UserData } from '../providers/user-data';
-import { FeedPage } from '../pages/feed/feed';
 import { Storage } from '@ionic/storage';
-import firebase from 'firebase';
 import {UsersProvider} from '../providers/users/users';
 import { TabsPage } from '../pages/tabs/tabs';
-// import { Socket } from 'ng-socket-io';
 declare var Appsee:any;
 
 
@@ -30,6 +27,7 @@ export class MyApp {
     public storage:Storage,
     private httpProvider:UsersProvider
 ) {
+
         this.userData.hasLoggedIn().then((hasLoggedIn) => {
       
                 if(hasLoggedIn){
@@ -37,30 +35,19 @@ export class MyApp {
                 }
                 else{
                   this.rootPage = HomePage;
-                  
-
                 }
         }); 
         
         this.httpProvider.setToken().subscribe(data =>{
-          console.log("Token", data.auth_token );
           localStorage.setItem('token', data.auth_token);
        
         });
           platform.ready().then((readySource) => {
-              
-
             console.log("Platform Ready from ", readySource);
             statusBar.overlaysWebView(false);
             statusBar.backgroundColorByHexString('#f4512c');
             Appsee.start("e81f4eb7b562458b80bbd8fb1f6130dc");
          
          });
-
-
-            
     }
-
-
-    
 }
--- src/pages/donate-feed-back/donate-feed-back.ts
+++ src/pages/donate-feed-back/donate-feed-back.ts
@@ -1,134 +1,144 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
-import { ThankYouPage } from '../thank-you/thank-you';
-import { UsersProvider } from '../../providers/users/users';
-import { IssueScreenPage } from '../issue-screen/issue-screen';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, ModalController} from 'ionic-angular';
+import {ThankYouPage} from '../thank-you/thank-you';
+import {UsersProvider} from '../../providers/users/users';
+import {IssueScreenPage} from '../issue-screen/issue-screen';
 import {ThemeableBrowser} from "@ionic-native/themeable-browser";
 import * as constants from '../../constants/constants';
 
 
 @IonicPage()
 @Component({
-  selector: 'page-donate-feed-back',
-  templateUrl: 'donate-feed-back.html',
+    selector: 'page-donate-feed-back',
+    templateUrl: 'donate-feed-back.html',
 })
 export class DonateFeedBackPage {
-  isenabled:boolean=false;
-  url:any;
-  value:any;
-  endpoint:any = 'actions';
-  data:any = [{
-    user_id: '',
-    title: '',
-    short_desc: '',
-    representative_id: '',
-    action_type_id: '',
-    goal_id: ''
-  }]; 
-  objectiveID:any;
+    isenabled: boolean = false;
+    url: any;
+    value: any;
+    endpoint: any = 'actions';
+    data: any = [{
+        user_id: '',
+        title: '',
+        short_desc: '',
+        representative_id: '',
+        action_type_id: '',
+        goal_id: ''
+    }];
+    objectiveID: any;
 
-  constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams,
-    public modalCtrl: ModalController,
-    private themeableBrowser: ThemeableBrowser,
-    private httpProvider: UsersProvider) {
-      this.url = navParams.get('iframeUrl');
-      this.openWebpage(this.url);
-      this.data.goal_id = navParams.get('goalID');
-      this.data.representative_id = navParams.get('repID');
-      this.data.action_type_id = '500f35fc-9338-4f1d-bdc8-13302afa33e7';
-      this.data.title = 'donat';
-      this.objectiveID = navParams.get('objectiveID');
-      this.httpProvider.returnRallyUserId().then( user => {
-        this.data.user_id = user.apiRallyID;
-      });
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DonateFeedBackPage');
-  }
-
-  sendActions($event){
-    this.isenabled = true;
-    console.log($event);
-    
-    let clickedElement = $event.target || $event.srcElement;
-
-    if( clickedElement.nodeName === "BUTTON" ) {
-
-      let isCertainButtonAlreadyActive = clickedElement.parentElement.querySelector(".active");
-      // if a Button already has Class: .active
-      if( isCertainButtonAlreadyActive ) {
-        isCertainButtonAlreadyActive.classList.remove("active");
-      }
-
-      clickedElement.className += " active";
-    }
-
-  }
-
-  streakModal() {
-    let modal = this.modalCtrl.create(ThankYouPage);
-    modal.onDidDismiss(() => {
-      this.navCtrl.popTo(this.navCtrl.getByIndex(0), {animate:true,animation:'transition',duration:500,direction:'back'});
-    });
-    modal.present();
-  }
-
-  openWebpage(url: string) {
-    const options = constants.themeAbleOptions;
-    const browser = this.themeableBrowser.create(url, '_system', options);
-
-    browser.on("loadstop")
-        .subscribe(
-        () => {
-            browser.insertCss({
-                code: "body, html {padding-top: 20px!important;} header .rn-ipm5af{top: 16px !important; margin-top: 0 !important;} main{overflow:hidden}"
-            })
-        },
-        err => {
-            console.log("InAppBrowser Loadstop Event Error: " + err);
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public modalCtrl: ModalController,
+        private themeableBrowser: ThemeableBrowser,
+        private httpProvider: UsersProvider) {
+        this.url = navParams.get('iframeUrl');
+        this.openWebpage(this.url);
+        this.data.goal_id = navParams.get('goalID');
+        this.data.representative_id = navParams.get('repID');
+        this.data.action_type_id = '500f35fc-9338-4f1d-bdc8-13302afa33e7';
+        this.data.title = 'donat';
+        this.objectiveID = navParams.get('objectiveID');
+        this.httpProvider.returnRallyUserId().then(user => {
+            this.data.user_id = user.apiRallyID;
         });
-
-      browser.on('closePressed').subscribe(data => {
-          browser.close();
-      })
-  }
-
-  errorModal(){
-    let modal = this.modalCtrl.create(IssueScreenPage);
-      modal.onDidDismiss((val) => {
-          this.navCtrl.popTo(this.navCtrl.getByIndex(0), {animate:true,animation:'transition',duration:500,direction:'back'});
-      });
-    modal.present();
-  }
-
-  getValue(value){
-    console.log(value);
-    this.value = value;
-
-  }
-
-  addAction(){
-    this.httpProvider.addAction(this.endpoint, this.data);
-  }
-
-  back(){
-    this.navCtrl.pop();
-  }
-
-  submit(){
-  
-    console.log("Value", this.value);
-    if(this.value === 'success'){
-      this.streakModal();
-      this.addAction();
-    }else if(this.value === 'fail'){
-      this.errorModal();
-    }else{
-      this.back();
     }
-  }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad DonateFeedBackPage');
+    }
+
+    sendActions($event) {
+        this.isenabled = true;
+        console.log($event);
+
+        let clickedElement = $event.target || $event.srcElement;
+
+        if (clickedElement.nodeName === "BUTTON") {
+
+            let isCertainButtonAlreadyActive = clickedElement.parentElement.querySelector(".active");
+            // if a Button already has Class: .active
+            if (isCertainButtonAlreadyActive) {
+                isCertainButtonAlreadyActive.classList.remove("active");
+            }
+
+            clickedElement.className += " active";
+        }
+
+    }
+
+    streakModal() {
+        let modal = this.modalCtrl.create(ThankYouPage);
+        modal.onDidDismiss(() => {
+            this.navCtrl.popTo(this.navCtrl.getByIndex(0), {
+                animate: true,
+                animation: 'transition',
+                duration: 500,
+                direction: 'back'
+            });
+        });
+        modal.present();
+    }
+
+    openWebpage(url: string) {
+        const options = constants.themeAbleOptions;
+        const browser = this.themeableBrowser.create(url, '_system', options);
+
+        browser.on("loadstop")
+            .subscribe(
+                () => {
+                    browser.insertCss({
+                        code: "body, html {padding-top: 20px!important;} header .rn-ipm5af{top: 16px !important; margin-top: 0 !important;} main{overflow:hidden}"
+                    })
+                },
+                err => {
+                    console.log("InAppBrowser Loadstop Event Error: " + err);
+                });
+
+        browser.on('closePressed').subscribe(data => {
+            browser.close();
+        })
+    }
+
+    errorModal() {
+        let modal = this.modalCtrl.create(IssueScreenPage);
+        modal.onDidDismiss((val) => {
+            this.navCtrl.popTo(this.navCtrl.getByIndex(0), {
+                animate: true,
+                animation: 'transition',
+                duration: 500,
+                direction: 'back'
+            });
+        });
+        modal.present();
+    }
+
+    getValue(value) {
+        console.log(value);
+        this.value = value;
+
+    }
+
+    addAction() {
+        this.httpProvider.addAction(this.endpoint, this.data);
+    }
+
+    back() {
+        this.navCtrl.pop();
+    }
+
+    submit() {
+
+        console.log("Value", this.value);
+        if (this.value === 'success') {
+            this.streakModal();
+            this.addAction();
+        } else if (this.value === 'fail') {
+            this.errorModal();
+        } else {
+            this.back();
+        }
+    }
 
 }
