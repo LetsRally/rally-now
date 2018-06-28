20de436b18da9192a4b7e8c02483eb78038cbbfa
Viacheslav Strukov <viacheslav.strukov@p2h.com>
2018-06-28 17:16:23 +0300

    Search screen - pipe for dates

--- src/pipes/date-transform/date-transform.ts
+++ src/pipes/date-transform/date-transform.ts
@@ -1,15 +1,25 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 
 @Pipe({
-  name: 'dateTransform',
+    name: 'dateTransform',
 })
 export class DateTransformPipe implements PipeTransform {
 
-  transform(value: string, ...args) {
-    let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    let date = new Date(value);
-    let transformedDate = date.getDate() + ' ' + month[date.getMonth()];
-    return transformedDate;
-  }
+    transform(value: string, endDateValue) {
+        let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        let date = new Date(value);
+        let transformedDate = month[date.getMonth()] + ' ' + date.getDate();
+        let transformedEndDate;
+        if (endDateValue) {
+            let endDate = new Date(endDateValue);
+            transformedEndDate = month[endDate.getMonth()] + ' ' + endDate.getDate();
+        }
+
+        if(transformedDate !== transformedEndDate) {
+            return transformedDate + ' - ' + transformedEndDate;
+        }
+
+        return transformedDate;
+    }
 }
